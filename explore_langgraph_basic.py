f92ab5588d3e7c886fa1b69e5472599dd9a6631b
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-09-30 14:49:50 +0530

    simplify the code

--- explore_langgraph_basic.py
+++ explore_langgraph_basic.py
@@ -1,49 +1,47 @@
 from dotenv import load_dotenv
-from typing import Annotated, TypedDict
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
-from langchain_openai import ChatOpenAI
-from langchain_core.runnables import Runnable
+load_dotenv()
+#----------------------------------------------------------------------
 
+from langchain_openai import ChatOpenAI
+llm = ChatOpenAI()
+#----------------------------------------------------------------------
+
+from typing import TypedDict, Annotated
+from langgraph.graph.message import add_messages
 class State(TypedDict):
     messages: Annotated[list, add_messages]
+#----------------------------------------------------------------------
 
-class Chatbot:
-    def __init__(self):
-        load_dotenv()
-        self.llm = ChatOpenAI()
-        self._build_graph()
-        self.graph = self._build_graph()
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+#----------------------------------------------------------------------
 
-    def _build_graph(self) -> Runnable:
-        graph_builder = StateGraph(State)
-        graph_builder.add_node('chatbot', self.chatbot)
-        graph_builder.add_edge(START, 'chatbot')
-        graph_builder.add_edge('chatbot', END)
-        return graph_builder.compile()
+from langgraph.graph import StateGraph, START, END
+graph_builder = StateGraph(State)
+# The first argument is the unique node name
+# The second argument is the function or object that will be called whenever
+# the node is used.
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+graph = graph_builder.compile()
+#----------------------------------------------------------------------
 
-    def chatbot(self, state: State) -> State:
-        return {'messages': [self.llm.invoke(state['messages'])]}
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [("user", user_input)]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
 
-    def stream_graph_updates(self, user_input: str):
-        for event in self.graph.stream({"messages": [("user", user_input)]}):
-            for value in event.values():
-                print("Assistant:", value["messages"][-1].content)
 
-    def run(self):
-        while True:
-            try:
-                user_input = input("User: ")
-                if user_input.lower() in ["quit", "exit", "q"]:
-                    print("Goodbye!")
-                    break
-                self.stream_graph_updates(user_input)
-            except:
-                user_input = "What do you know about LangGraph?"
-                print("User: " + user_input)
-                self.stream_graph_updates(user_input)
-                break
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
 
-if __name__ == "__main__":
-    chatbot = Chatbot()
-    chatbot.run()
+        stream_graph_updates(user_input)
+    except:
+        print("An error occurred. Exiting...")
+        break
+#----------------------------------------------------------------------    
